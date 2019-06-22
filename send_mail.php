153030a22187d6b480f07d93b602ea7c68a1f981
Energy90 <mcmalindi@gmail.com>
2019-06-22 19:35:30 +0200

    Added new code and deleted some code and images

--- send_mail.php
+++ send_mail.php
@@ -1,14 +1,39 @@
 <?php
-
+$firstName = $_POST["username"];
 $email = $_POST["user_mail"];
 $message = $_POST["user_message"];
 
+// begin validating name
+if(empty($firstName))
+{
+    echo "
+    <div class='alert alert-danger'>
+    <a href='#' class='close' data-dismiss='alert' arial-label='close'>&times</a>Pleasse enter your name...!
+    </div>
+    ";
+    exit();
+}
+else
+{
+    //validate method for fields
+    $firstName = test_input($firstName);
+    // check if name contains letters and white space
+    if(!preg_match("/^[a-zA-Z ]*$/", $firstName))
+    {
+        echo "
+        <div class='alert alert-danger'>
+        <a href='#' class='close' data-dismiss='alert' arial-label='close'>&times</a>This $firstName is not valid...!
+        </div>
+        ";
+        exit();
+    }
+}
 // begin validating email
 if(empty($email))
 {
     echo "
     <div class='alert alert-danger'>
-    <a href='#' class='close' data-dismiss='alert' arial-label='close'>&times</a><b>Please enter your email...!</b>
+    <a href='#' class='close' data-dismiss='alert' arial-label='close'>&times</a>Please enter your email...!
     </div>
     ";
     exit();
@@ -22,7 +47,7 @@ else
     {
         echo "
         <div class='alert alert-warning'>
-        <a href='#' class='close' data-dismiss='alert' arial-label='close'>&times</a><b>This $email is not valid.</b>
+        <a href='#' class='close' data-dismiss='alert' arial-label='close'>&times</a>This $email is not valid.
         </div>
         ";
         exit();
@@ -34,7 +59,7 @@ if (empty($message))
 {
     echo "
     <div class='alert alert-danger'>
-    <a href='#' class='close' data-dismiss='alert' arial-label='close'>&times</a><b>Please tell me a litle about your project.</b>
+    <a href='#' class='close' data-dismiss='alert' arial-label='close'>&times</a>Please tell me a litle about your project.
     </div>
     ";
     exit();
@@ -47,5 +72,66 @@ function test_input($data)
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
+}
+
+$to = "mcmalindi@gmail.com";
+$subject = "My Projects";
+$from = "From: ".$email."\r\n";
+$user_message = "
+<html>
+<head>
+<title>Hello </title>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+body
+{
+color: #505962;
+font-family: 'Roboto', sans-serif;
+padding: 50px;
+}
+.names
+{
+font-weight: 300;
+text-transform: bold;
+color: green;
+}
+
+</style>
+</head>
+<body>
+<div>
+<h2 style='text-align: center'>My Projects</h2>
+<table>
+<tr><td>From: <span class='names'>$from</span></td></tr>
+<tr><td>Name: <span class='names'>$firstName</span></td></tr>
+</table>
+<p>
+$message
+</p>
+</div>
+</body>
+</html>
+";
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= $from;
+
+$sent = mail($to, $subject, $user_message, $headers);
+if($sent)
+{
+    echo "
+    <div class='alert alert-danger'>
+    <a href='#' class='close' data-dismiss='alert' arial-label='close'>&times;</a>Message send successfully, I'll contact you soon. Thanky'
+    </div>
+    ";
+}
+else
+{
+    echo "
+    <div class='alert alert-danger'>
+    <a href='#' class='close' data-dismiss='alert' arial-label='close'>&times;</a>There has been an error sending your message. Please try again later.'
+    </div>
+    ";
 }
 ?>
